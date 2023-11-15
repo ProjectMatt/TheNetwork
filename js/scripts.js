2a3fa4c64d9558ded201139516d98622c033f6b0
Matt <119772279+ProjectMatt@users.noreply.github.com>
2023-11-15 13:13:29 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+function enrollInClass(classId) {
+    alert(`Enrolling in class ${classId}`);
+}
+
+function navigateToLesson(lessonId) {
+
+    alert(`Navigating to lesson ${lessonId}`);
+}
